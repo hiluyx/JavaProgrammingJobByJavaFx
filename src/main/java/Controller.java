ca357b16d02c244dfc4de5428659f652c4dd11af
hiluyx <hiluyx@users.github.com>
2020-03-18 12:38:44 +0800

    Initial commit

---
+++ src/main/java/Controller.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    @FXML
+    private Button hello;
+
+
+    @FXML
+    public void handleButtonAction(ActionEvent actionEvent) {
+        hello.setText("Hello World, but sky is the best cool Super Saia!");
+    }
+}
